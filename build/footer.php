5c9332317c3a863bdbb3e5ca536136145ce971e7
Lenin Hasda <leninhasda@gmail.com>
2017-04-16 11:46:02 +0600

    made a backup of previous site

--- build/footer.php
+++
@@ -1,5 +0,0 @@
-    <!--Import jQuery before materialize.js-->
-    <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="bower_components/materialize/dist/js/materialize.min.js"></script>
-</body>
-</html>
